eafaf3bb4ec9ed870a06d08acdc387d3596bc0e9
José García <josegarcia81@hotmail.com>
2021-01-28 15:56:59 -0600

    Primer entidad

---
+++ SistemaControlCuentas.Entidades/Departamentos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaControlCuentas.Entidades
+{
+    class Departamentos
+    {
+        public int Id { get; set; }
+    }
+}
