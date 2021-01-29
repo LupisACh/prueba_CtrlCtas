f9573a4284cda9459b7bc0ca3a2cd5c3ccd5f5da
José García <josegarcia81@hotmail.com>
2021-01-29 13:56:06 -0600

    Entidades Deptos, Ctas, Beneficiarios

---
+++ SistemaControlCuentas.Entidades/Beneficiarios.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaControlCuentas.Entidades
+{
+    public class Beneficiarios
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Curp { get; set; }
+
+    }
+}
---
+++ SistemaControlCuentas.Entidades/Cuenta.cs
@@ -0,0 +1,16 @@
+﻿namespace SistemaControlCuentas.Entidades
+{
+    public class Cuenta
+    {
+        public int IdCuenta { get; set; }
+        public string NumeroCuenta { get; set; }
+        public string NombreCuetna { get; set; }
+        public string Alias { get; set; }
+        public string Descripcion { get; set; }
+        public double MontoTotal { get; set; }
+        public double MontoEjercido { get; set; }
+        public string EjercicioFiscal { get; set; }
+        public short Activa { get; set; }
+        public short IdDepartamento { get; set; }
+    }
+}
